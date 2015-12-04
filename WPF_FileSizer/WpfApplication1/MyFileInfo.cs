61ad56851b453fe6c0588b5346362c929f1b471c
Carver Anglin <anglincarver@gmail.com>
2015-12-04 00:08:30 -0700

    merged and updated MyFileInfo to work caclalate correctly for all file types

--- WPF_FileSizer/WpfApplication1/MyFileInfo.cs
+++ WPF_FileSizer/WpfApplication1/MyFileInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +11,50 @@ namespace WpfApplication1
     class MyFileInfo
     {
         public string Name { get; private set; }
-        public int Size { get; private set; }
-        public MyDirectoryInfo Parent { get; private set; }
+        public double Size { get; private set; }
+        public MyFileInfo Parent { get; private set; }
 
-        public MyFileInfo(string path)
+        public IList<MyFileInfo> SubFiles { get; set; }
+
+        private MyFileInfo(string path, MyFileInfo parent)
         {
+            Name = path.Substring(path.LastIndexOf("\\") + 1);
+            Parent = parent;
+            SubFiles = new List<MyFileInfo>();
+            FileAttributes attr = File.GetAttributes(path);
 
+            if (attr.HasFlag(FileAttributes.Directory))
+            {
+                foreach (string dir in Directory.GetDirectories(path))
+                {
+                    SubFiles.Add(new MyFileInfo(dir, this));
+                }
+
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    SubFiles.Add(new MyFileInfo(file, this));
+                }
+
+                foreach (MyFileInfo f in SubFiles)
+                {
+                    Size += f.Size;
+                }
+            }
+            else
+            {
+               Size = new FileInfo(path).Length;
+                Size /= 1024;
+            }
+
+
+
+        }
+
+        public MyFileInfo(String path)
+        {
+            Name = "__Parent";
+            SubFiles = new List<MyFileInfo>();
+            SubFiles.Add(new MyFileInfo(path, this));
         }
     }
 }
