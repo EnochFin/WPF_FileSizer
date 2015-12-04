e55e90f3d2a68f5115bb2897b0d5cf9b189c84e6
Carver Anglin <anglincarver@gmail.com>
2015-12-04 09:59:33 -0700

    size converted to the best unit

---
+++ WPF_FileSizer/WpfApplication1/BytesToUnitsConverter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfApplication1
+{
+    public class BytesToUnitsConverter : IValueConverter
+    {
+        private static string[] units = new[] {"b", "kb", "mb", "gb", "tb"};
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double size = System.Convert.ToDouble(value);
+            int unit = 0;
+            while (size > 1024)
+            {
+                size /= 1024;
+                unit++;
+            }
+            return size.ToString("F") + " " + units[unit];
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
